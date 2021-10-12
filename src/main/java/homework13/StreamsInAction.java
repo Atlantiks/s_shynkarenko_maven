4d28fe92cdc693dab0cc64dcbe8a04b805399114
Atlantiks <atlantiks@bigmir.net>
2021-10-12 15:34:00 +0300

    HW #13: Task 2-3 completed and covered with tests.

--- src/main/java/homework13/StreamsInAction.java
+++ src/main/java/homework13/StreamsInAction.java
@@ -1,14 +1,36 @@
 package homework13;
 
 import java.util.Collection;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class StreamsInAction {
 
     public static double getAverageOfCollection(Collection<Integer> collection) {
+        // не добавил проверку isPresent для лаконичности кода, но добавлю, если это критично
         return collection
                 .stream()
                 .mapToInt(Integer::intValue)
                 .average()
                 .getAsDouble();
+    }
+
+    public static Collection<String> getCollectionOfEnglishWordsInUpperCase(Collection<String> collection, String typeOfReturnedCollection) {
+        Stream<String> myStream  = collection
+                .stream()
+                .filter(n -> n.matches("^[AEIOUaeiou]+.*"))
+                .map(String::toUpperCase);
+        switch(typeOfReturnedCollection) {
+            case "List" : return myStream.collect(Collectors.toList());
+            case "Set" :  return myStream.collect(Collectors.toSet());
+            default : return null;
+        }
+    }
+
+    public static void filterLowerCaseAndPrint(Collection<String> collection){
+        collection.stream()
+                .filter(n -> n.matches("[a-zа-я]*"))
+                .filter(n -> n.length() == 4)
+                .forEach(System.out::println);
     }
 }
--- src/test/java/homework13/StreamsInActionTest.java
+++ src/test/java/homework13/StreamsInActionTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 public class StreamsInActionTest {
     static List<Integer> myIntegers= new ArrayList<>();
@@ -19,5 +20,43 @@ public class StreamsInActionTest {
                 myIntegers.stream().reduce(Integer::sum).get() / (double) myIntegers.size(),
                         StreamsInAction.getAverageOfCollection(myIntegers),
                    0.01);
+    }
+
+    @Test
+    public void testGetCollectionOfEnglishWordsInUpperCase() {
+        myStrings.add("Apple");
+        myStrings.add("Apple");
+        myStrings.add("Яблоко");
+        myStrings.add("Банан");
+        myStrings.add("Oreo");
+        myStrings.add("Груша");
+        myStrings.add("Pineapple");
+        myStrings.add("Coconut");
+        myStrings.add("Киви");
+        myStrings.add("U");
+        myStrings.add("Арбуз");
+        // добавляем слово, начинающееся латинской буквой и содержащее далее кириллицу
+        // для проверки правильности подобранного регулярного выражения
+        myStrings.add("Aрбуз");
+
+
+        List<String> myList = (List<String>) StreamsInAction.getCollectionOfEnglishWordsInUpperCase(myStrings,"List");
+        Set<String> mySet = (Set<String>) StreamsInAction.getCollectionOfEnglishWordsInUpperCase(myStrings,"Set");
+        System.out.println(myList);
+        System.out.println(mySet);
+        Assert.assertEquals(7, myList.size());
+        Assert.assertEquals(6, mySet.size());
+    }
+
+    @Test
+    public void testFilterLowerCaseAndPrint() {
+        myStrings.add("яблоко");
+        myStrings.add("мама");
+        myStrings.add("iPhone");
+        myStrings.add("Mitsubishi");
+        myStrings.add("daddy");
+        myStrings.add("aunt");
+
+        StreamsInAction.filterLowerCaseAndPrint(myStrings);
     }
 }
